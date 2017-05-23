39afd919c61077ae3f6b4bac3673209cf4801c84
dimabresky <dimabresky@gmail.com>
2017-05-23 10:55:45 +0300

    добавлен файл с функциями

---
+++ lib/functions.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace travelsoft;
+
+if (!function_exists("ats")) {
+    /**
+     * Конвертирует массив в строку
+     * @param array $array
+     * @return string
+     */
+    function ats (array $array) : string {
+        return (string)base64_encode(gzcompress(serialize($array), 9));
+    }
+}
+
+if (!function_exists("sta")) {
+    /**
+     * Коневертирует строку в массив
+     * @param string $str
+     * @return array
+     */
+    function sta (string $str) : array {
+        return (array)unserialize(gzuncompress(base64_decode($str)));
+    }
+}
