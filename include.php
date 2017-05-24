f95ff4934c8f28c4a66adaf6c9652e94809f56c5
dimabresky <dimabresky@gmail.com>
2017-05-24 16:44:40 +0300

    Даблено подключение классов

---
+++ include.php
@@ -0,0 +1,17 @@
+<?php
+
+$classes = array(
+    
+    "travelsoft\\currency\\Converter" => "lib/Converter.php",
+    "travelsoft\\currency\\Course" => "lib/Course.php",
+    "travelsoft\\currency\\Currency" => "lib/Currency.php",
+    "travelsoft\\currency\\Result" => "lib/Result.php",
+    "travelsoft\\currency\\Settings" => "lib/Settings.php",
+    
+    
+    "travelsoft\\currency\\stores\\Courses" => "lib/Courses.php",
+    "travelsoft\\currency\\stores\\Currencies" => "lib/Currencies.php",
+    
+    "travelsoft\\currency\\interfaces\\Getter" => "lib/Getter.php",    
+    "travelsoft\\currency\\interfaces\\Store" => "lib/Store.php"    
+);
