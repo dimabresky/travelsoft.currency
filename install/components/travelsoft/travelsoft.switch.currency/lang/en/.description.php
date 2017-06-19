bdcf52ba72b9712f500bee361cf2bdfd2cd3591c
dimabresky <dimabresky@gmail.com>
2017-06-19 09:50:32 +0300

    update

--- install/components/travelsoft/travelsoft.switch.currency/lang/en/.description.php
+++
@@ -1,4 +0,0 @@
-<?
-$MESS ['TRAVELSOFT_CURRENCY_COMP_NAME'] = "Currency switch";
-$MESS ['TRAVELSOFT_CURRENCY_COMP_DESC'] = "Form for switch currency";
-?>
--- install/components/travelsoft/travelsoft.switch.currency/lang/ru/.description.php
+++
@@ -1,4 +0,0 @@
-<?
-$MESS ['TRAVELSOFT_CURRENCY_COMP_NAME'] = "Переключение валюты";
-$MESS ['TRAVELSOFT_CURRENCY_COMP_DESC'] = "Форма для переключения валюты на сайте";
-?>
