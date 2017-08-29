1c87fa87d81b9b0299ed1ff6f0aed71ed469d7cc
dimabresky <dimabresky@gmail.com>
2017-08-29 10:58:36 +0000

    include.php отредактирован онлайн на Bitbucket

--- include.php
+++ include.php
@@ -13,4 +13,4 @@ $classes = array(
     "travelsoft\\CREventsHandlers" => "lib/CREventsHandlers.php"
 );
 CModule::AddAutoloadClasses("travelsoft.currency", $classes);
-require_once 'lib/functions.php'
+require_once 'lib/functions.php';
