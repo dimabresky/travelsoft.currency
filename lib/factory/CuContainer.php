dcaccba3ae61f670520fb76193df068ad14898c8
Дмитрий <dimabresky@gmail.com>
2021-09-21 23:38:58 +0300

    fix cross course calculation

--- lib/factory/CuContainer.php
+++ lib/factory/CuContainer.php
@@ -8,53 +8,63 @@ namespace travelsoft\currency\factory;
  * @author dimabresky
  * @copyright (c) 2017, travelsoft
  */
-class CuContainer extends \travelsoft\currency\interfaces\Factory{
-    
+class CuContainer extends \travelsoft\currency\interfaces\Factory {
+
     /**
      * @staticvar type $instances
      * @param \travelsoft\currency\Currency $currency
      * @return \travelsoft\currency\CuContainer
      */
-    public static function getInstance (\travelsoft\currency\Currency $currency = null) : \travelsoft\currency\CuContainer{
-       
+    public static function getInstance(\travelsoft\currency\Currency $currency = null): \travelsoft\currency\CuContainer {
+
         static $instances = null;
-        
+
         $hesh = md5(serialize(func_get_args()));
-        
+
         if (!$instances[$hesh]) { // генерируем класс контейнера валют
-            
             if (!$currency) {
                 $currency = Currency::getInstance();
             }
-            
+
             $arCourses = (array) $currency->courses;
-            
+
             $arrCourses = $arCourses;
-            
+
             $cuContainer = new \travelsoft\currency\CuContainer;
-            
+
             $cuContainer->addCurrency($currency);
-            
+
             $cuContainer->setCurrentCurrencyIso($currency->ISO);
-            
-            unset($arCourses[$currency->ISO]);
-            
+
+            $arCurrencies = \travelsoft\currency\stores\Currencies::get();
+            $arCourse = current(\travelsoft\currency\stores\Courses::get(array("filter" => array("ID" => \travelsoft\currency\Settings::currentCourseId()))));
+
+            $baseIso = (string) $arCurrencies[$arCourse["UF_BASE_ID"]]["UF_ISO"];
+
             foreach ($arCourses as $ISO => $course) {
 
                 $tmpCurrency = new \travelsoft\currency\Currency($ISO);
 
                 foreach ($arrCourses as $IISO => $ccourse) {
 
-                    $tmpCurrency->addCourse($IISO, new \travelsoft\currency\Course($ccourse->value / $course->value, $course->date));
+                    $comission = $ccourse->comission;
+                    $val = $ccourse->value / $course->value;
+
+                    if ($ISO != $baseIso && $IISO != $baseIso && $ISO != $IISO) {
+                        $cval = $comission / 100;
+                        $val = $ccourse->sourceValue / $course->sourceValue;
+                        $val = $val + ($val * $cval);
+                    }
+                    $tmpCurrency->addCourse($IISO, new \travelsoft\currency\Course($val, $course->date, $comission));
                 }
-                
+
                 $cuContainer->addCurrency($tmpCurrency);
             }
-            
+
             $instances[$hesh] = $cuContainer;
         }
-        
+
         return $instances[$hesh];
-        
     }
+
 }
